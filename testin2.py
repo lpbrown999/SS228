3d09a618cc1bd004de3ae3b418d821df046768ce
lpbrown999 <lpbrown999@gmail.com>
2018-11-13 20:13:46 -0800

    removed unnecessary files

--- testin2.py
+++
@@ -1,8 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser(description='Example of libmelee in action')
-
-parser.add_argument('--logging','-l', action='store_true',
-                    help='Logging of Gamestates')
-parser.add_argument('--twoagents','-2',action ='store_true')
-args = parser.parse_args()
-print(args)
