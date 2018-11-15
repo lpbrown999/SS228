8c314209b0803c61147fa4fcdaa76305701d4a50
Jeremy Crowley <jcrowley@ucsc.edu>
2018-11-14 23:09:52 -0800

    beta function dictionary implemented

--- beta.py
+++ beta.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 # beta functions relating to our y value and x value
-def jumper_beta_xy(stateVal):
+def jumper_beta_xy_1(stateVal):
 
 	ax = stateVal[0]
 	ay = stateVal[1]
@@ -42,6 +42,55 @@ def jumper_beta_y(stateVal):
 	beta = np.array([ay**2,invay])
 
 	return beta
+
+def beta_jumper_xy_2(stateVal):
+
+	ax = stateVal[0]
+	ay = stateVal[1]
+
+	# cap inverse of x
+	if(ax < 0.1):
+		invax = 1000
+	else:
+		invax = 1/ax
+
+	# cap inverse of y
+	if(ay < 0.1):
+		invay = 1000
+	else:
+		invay = 1/ay
+
+	# set basis functions
+	beta = np.array([ax**3, ay**3, invax, invay])
+	
+	return beta
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Remeber to update this dictionary when adding a new beta function #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+betaDict = {"1":jumper_beta_xy_1,
+			"2": jumper_beta_y,
+			"3": beta_jumper_xy_2}
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Remeber to update this dictionary when adding a new beta function #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+
+# example of how to use this dicionary of function
+
+"""
+from beta import betaDict
+
+functionHandle = betaDict["3"]
+
+print(functionHandle(4))
+"""
+
 
 
 
